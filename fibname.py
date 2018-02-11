db62eb68572f0fcad33aa0846a55211f5e3bee99
Pat McDonald <patmcdonald@me.com>
2018-02-11 14:22:42 +0000

    Update fibname.py

--- fibname.py
+++ fibname.py
@@ -3,6 +3,21 @@
 # Original code by Ian McLoughlin
 # A program that displays Fibonacci numbers using people's names.
 # This script includes the python ord() function 
+'''My surname is McDonald
+
+The first letter M is number 77
+
+The last letter d is number 100
+
+Fibonacci number 177 is 4378519841510949178490918731459856482
+
+ord() is a built-in function of python 3.6
+
+From https://docs.python.org/3/library/functions.html#ord :
+
+"Given a string representing one Unicode character, return an integer representing the Unicode code point of that character. For example, ord('a') returns the integer 97 and ord('€') (Euro sign) returns 8364. This is the inverse of chr()."'''
+
+
 def fib(n):
   """This function returns the nth Fibonacci number."""
   i = 0
