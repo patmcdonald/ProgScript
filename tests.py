9d514657bc5872d3498dbcc9e22ed054ff21cf52
Pat McDonald <patmcdonald@me.com>
2018-02-11 13:21:00 +0000

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,1 +1,37 @@
+#Original code by Ian McLoughlin
+#Week 2: Programming and Scriping
+
+#cost = 14.99
+#taxperc = 23
+
+#tax = taxperc / 100
+
+#saleprc = cost * (1.0 + tax)
+
+#print("Sale price: %0.2f" % saleprc)
+
+i= 11
+print(i)
+
+j= 12
+print(j)
+
+k = i * j
+print(k)
+
+f = 12.34
+print(f)
+
+g = 0.99
+print(g)
+
+s = "Hello, World!"
+print(s)
+print(s[0])
+print(s[:5])
+print(s[7:12])
+
+l = [2,3,5,7,11]
+print(l)
+print(l[2])
 
