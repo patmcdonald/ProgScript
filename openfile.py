9e2d2544824d24452d6e53b74a6ab99e62b68fef
Pat McDonald <patmcdonald@me.com>
2018-04-06 21:52:19 +0100

    Update openfile.py

--- openfile.py
+++ openfile.py
@@ -1,5 +1,6 @@
 #Pat McDonald
 #Exercise 5: Programming and Scripting
+#Working with Fishers Iris dataset. Source: https://archive.ics.uci.edu/ml/datasets/iris
 
 with open("data/iris.csv") as f:
   contents = f.read()
