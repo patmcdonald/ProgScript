cd84da5232621d09b7bef46231ac63d75c511d0c
Pat McDonald <patmcdonald@me.com>
2018-04-06 21:56:58 +0100

    Getting to grips with merges and sync with GitHub

--- openfile.py
+++ openfile.py
@@ -3,10 +3,5 @@
 #Working with Fishers Iris dataset. Source: https://archive.ics.uci.edu/ml/datasets/iris
 
 with open("data/iris.csv") as f:
-<<<<<<< HEAD
   contents = f.read() 
   print(contents)
-=======
-  contents = f.read()
-  print(contents)
->>>>>>> 9e2d2544824d24452d6e53b74a6ab99e62b68fef
