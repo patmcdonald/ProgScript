a45cbd6dee66f4dd13b175eef465d4d8400202e2
Pat McDonald <patmcdonald@me.com>
2018-04-07 15:43:40 +0100

    Added comments

--- openfile.py
+++ openfile.py
@@ -2,6 +2,9 @@
 #Exercise 5: Programming and Scripting
 #Working with Fishers Iris dataset. Source: https://archive.ics.uci.edu/ml/datasets/iris
 
+#Open the iris dataset
 with open("data/iris.csv") as f:
+  #Loop through each line
   for line in f:
+    #Split and print each line of string values
     print(line.split(',' [:4]))
