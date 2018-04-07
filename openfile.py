0fa188a53042e550ff7b6353e3d615d2b903fe11
Pat McDonald <patmcdonald@me.com>
2018-04-07 21:48:51 +0100

    Added URL reference for code source

--- openfile.py
+++ openfile.py
@@ -7,7 +7,7 @@ with open("data/iris.csv") as f:
   #Loop through each line
   for line in f:
     #Split and print each line of string values
-    #Code by Mohamed Noor: 
+    #Code by Mohamed Noor;https://learnonline.gmit.ie/mod/forum/discuss.php?d=14986#p29763 
         line = line.replace(',', ' ')
         line = line.rstrip()
         print(line[:11], line[12:16].strip())
