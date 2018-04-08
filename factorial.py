e536b6ab1a836ea53adb77886f17425ea749c34c
Pat McDonald <patmcdonald@me.com>
2018-04-08 14:11:11 +0100

    Comments added

--- factorial.py
+++ factorial.py
@@ -1,7 +1,10 @@
 #Pat McDonald
 #Exercise 6: Functions
 
+#importing python math module
 import math
+#used input from collatz.py
 n = int(input("Type an integer: "))
+#declare variable, call factorial function
 factorial = math.factorial(n)
 print(factorial)
